c1987002028abf788de3fbc22cadd439921d9f81
Akshat Thirani <akshatthirani@gmail.com>
2015-11-07 23:49:38 -0500

    fixed bugs

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 angular.module('includeExample', ['ngAnimate'])
   .controller('ExampleController', ['$scope', function($scope) {
     $scope.templates =
-      [ { name: '20', url: 'playlists/20.html'},
-        { name: '22', url: 'playlists/22.html'},
-        { name: '24', url: 'playlists/24.html'},
-        { name: '26', url: 'playlists/26.html'},
-        { name: '28', url: 'playlists/28.html'},];
+      [ { name: '20 spm', url: 'playlists/20.html'},
+        { name: '22 spm', url: 'playlists/22.html'},
+        { name: '24 spm', url: 'playlists/24.html'},
+        { name: '26 spm', url: 'playlists/26.html'},
+        { name: '28 spm', url: 'playlists/28.html'},];
     $scope.template = $scope.templates[0];
   }]);
 })(window.angular);
